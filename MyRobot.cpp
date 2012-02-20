5b3f3ffc1c957bc1b3be5fbec71fbf305d7f451d
Erik Madson <3rik.omni@gmail.com>
2012-02-20 13:06:03 -0600

    New Code!!!

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,55 +1,42 @@
 #include "WPILib.h"
 #include "math.h"
-/**
- * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
-
+	Joystick stick;
+	Jaguar FL,FR,BL,BR;
 public:
 	RobotDemo(void):
-		myRobot(3, 4, 5, 6),	// these must be initialized in the same order
-		stick(1, 3, 12)		// as they are declared above.
+		stick(1, 3, 12),
+		FL(3),
+		FR(5),
+		BL(4),
+		BR(6)
 	{
-		myRobot.SetInvertedMotor(myRobot.kFrontLeftMotor, true);
-		myRobot.SetInvertedMotor(myRobot.kRearLeftMotor, true);
-		myRobot.SetExpiration(0.1);
 	}
 
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
+#include "mecanumDrive.h"
+
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
 	}
-
-	/**
-	 * Runs the motors with arcade steering. 
-	 */
 	void OperatorControl(void)
 	{
-		myRobot.SetSafetyEnabled(true);
-		DriverStationLCD *dsLCD = DriverStationLCD::GetInstance();
+		DriverStationLCD *output = DriverStationLCD::GetInstance();
 		while (IsOperatorControl())
 		{
-			float xJoy = stick.GetRawAxis(1);
+			float xJoy = - stick.GetRawAxis(1);
 			float yJoy = - stick.GetRawAxis(2);
 			float twistJoy = stick.GetRawAxis(3);
-			xJoy = xJoy * fabs(xJoy);			
-			yJoy = yJoy * fabs(yJoy);			
+			
+			xJoy = xJoy * fabs(xJoy) * fabs(xJoy);			
+			yJoy = yJoy * fabs(yJoy) * fabs(yJoy);
+			twistJoy = twistJoy * fabs(twistJoy) * fabs(twistJoy);
+			
 			float magJoy = sqrt(xJoy * xJoy + yJoy * yJoy);
+			
 			float angleJoy;
 			if (yJoy != 0)
-				angleJoy = 90 - (yJoy / fabs(yJoy)) * (acos(xJoy / magJoy) * 180 / acos(-1));
+				angleJoy = 90 - yJoy / fabs(yJoy) * acos(xJoy / magJoy);
 			else
 				if (yJoy >= 0)
 					angleJoy = 90;
@@ -57,25 +44,24 @@ public:
 					angleJoy = - 90;
 			if (angleJoy > 180)
 				angleJoy -= 360;
-			if (magJoy > 1.0)
-				magJoy = 1.0;
-			dsLCD->Printf(DriverStationLCD::kMain_Line6, 9, "%7.1f", xJoy);
-			dsLCD->Printf(DriverStationLCD::kUser_Line2, 9, "%7.1f", yJoy);
-			dsLCD->Printf(DriverStationLCD::kUser_Line3, 9, "%7.1f", twistJoy);
-			dsLCD->Printf(DriverStationLCD::kUser_Line4, 9, "%7.1f", magJoy);
-			dsLCD->Printf(DriverStationLCD::kUser_Line5, 9, "%7.1f", angleJoy);
-			dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Axis 1");
-			dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Axis 2");
-			dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "Axis 3");
-			dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "Magnitude");
-			dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "Angle");
-			dsLCD->UpdateLCD();
-			 
-			myRobot.HolonomicDrive(magJoy, angleJoy, twistJoy);
-			Wait(0.001);				// wait for a motor update time
+			
+			mecanumDrive(magJoy, angleJoy, twistJoy); 
+			
+			
+			
+			output->Printf(DriverStationLCD::kMain_Line6, 9, "%7.1f", xJoy);
+			output->Printf(DriverStationLCD::kUser_Line2, 9, "%7.1f", yJoy);
+			output->Printf(DriverStationLCD::kUser_Line3, 9, "%7.1f", twistJoy);
+			output->Printf(DriverStationLCD::kUser_Line4, 9, "%7.1f", magJoy);
+			output->Printf(DriverStationLCD::kUser_Line5, 9, "%7.1f", angleJoy);
+			output->Printf(DriverStationLCD::kUser_Line1, 1, "Axis 1");
+			output->Printf(DriverStationLCD::kUser_Line2, 1, "Axis 2");
+			output->Printf(DriverStationLCD::kUser_Line3, 1, "Axis 3");
+			output->Printf(DriverStationLCD::kUser_Line4, 1, "Magnitude");
+			output->Printf(DriverStationLCD::kUser_Line5, 1, "Angle");
+			output->UpdateLCD();
+			
+			
 		}
 	}
 };
-
-START_ROBOT_CLASS(RobotDemo);
-
---
+++ mecanumDrive.h
@@ -0,0 +1,50 @@
+float Limit(float num)
+{
+	if (num > 1.0)
+	{
+		return 1.0;
+	}
+	if (num < -1.0)
+	{
+		return -1.0;
+	}
+	return num;
+}
+
+void Normalize(double *wheelSpeeds)
+{
+	double maxMagnitude = fabs(wheelSpeeds[0]);
+	INT32 i;
+	for (i=1; i<4; i++)
+	{
+		double temp = fabs(wheelSpeeds[i]);
+		if (maxMagnitude < temp) maxMagnitude = temp;
+	}
+	if (maxMagnitude > 1.0)
+	{
+		for (i=0; i<4; i++)
+		{
+			wheelSpeeds[i] = wheelSpeeds[i] / maxMagnitude;
+		}
+	}
+}
+
+void mecanumDrive(float magnitude, float direction, float rotation)
+{
+    magnitude = Limit(magnitude) * sqrt(2.0);
+    double dirInRad = (direction + acos(-1) / 4);
+    double cosD = cos(dirInRad);
+    double sinD = sin(dirInRad);
+    double wheelSpeeds[4];
+    wheelSpeeds[0] = sinD * magnitude + rotation;
+    wheelSpeeds[1] = cosD * magnitude - rotation;
+    wheelSpeeds[2] = cosD * magnitude + rotation;
+    wheelSpeeds[3] = sinD * magnitude - rotation;
+
+    Normalize(wheelSpeeds);
+    
+    FL.Set(- wheelSpeeds[0]);
+    FR.Set(wheelSpeeds[1]);
+    BL.Set(- wheelSpeeds[2]);
+    BR.Set(wheelSpeeds[3]);
+}
